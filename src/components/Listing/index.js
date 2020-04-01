a74620de3eb60ef7894d006bdc6546d006a0ee01
Matthew Taylor <mewtaylor@gmail.com>
2020-04-01 17:04:28 -0400

    Style listing component

--- src/components/Listing/index.js
+++ src/components/Listing/index.js
@@ -4,6 +4,8 @@ import moment from 'moment';
 import cs from './styles.module.css';
 
 export default ({listing}) => {
+  let hyperlink = "mailto:" + listing.emailAddress
+
   return (
     <article className={cs.listing}>
       <div className={cs.listingBody}>
@@ -16,11 +18,10 @@ export default ({listing}) => {
         <p>{listing.description}</p>
       </div>
       <div className={cs.actions}>
-        <div className={cs.location}>
-          <img src={require('./pin.svg')} className={cs.locationPin} alt="Location pin icon" />
-          {listing.location}
-        </div>
-        <button onClick={() => {}}>Meet need</button>
+        <a
+          className={cs.button}
+          href={hyperlink}
+        >Contact person</a>
       </div>
     </article>
   );
